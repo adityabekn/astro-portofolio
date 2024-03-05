0f9f6f186fdc0fef6241ebb3a4a1e64f4288b377
Aditya Bekti Nugroho <bekti.aditya16@gmail.com>
2024-03-05 22:55:59 +0700

    feat:custom tailwind and create Navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'main-dark': '#1C1B22',
+				'main-purple': '#FBFBFE'
+			}
+		},
 	},
 	plugins: [],
 }
